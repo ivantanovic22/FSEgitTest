db87eab1e89c394d9b5c1969ca1c085949461104
Ivan Tanovic <ITANOV200@CALEDONIAN.AC.UK>
2020-11-19 09:41:59 +0200

    Person class first commit

---
+++ Person.java
@@ -0,0 +1,16 @@
+public class Person 
+{
+
+private String firstname;
+private String othernames;
+private String familyname;
+
+
+	public Person()
+	{
+	firstname = " ";
+	othernames = " ";
+	familyname = " ";
+	}
+
+}// End of class.
